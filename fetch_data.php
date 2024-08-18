542fe9140f6a497aa528284bc4e890be2aad6a03
PEDADA PAVAN KUMAR <129097638+pavankumarpk2234@users.noreply.github.com>
2024-08-18 07:24:41 +0530

    Add files via upload

---
+++ fetch_data.php
@@ -0,0 +1,33 @@
+<?php
+// Database connection details
+$servername = "sql10.freesqldatabase.com";    // or your remote server name
+$username = "sql10726132";  // MySQL username
+$password = "8kdUmXhlxw";  // MySQL password
+$dbname = "sql10726132";    // Database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Query the database
+$sql = "SELECT * FROM your_table";
+$result = $conn->query($sql);
+
+// Fetch the data
+$data = array();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+// Return data as JSON
+echo json_encode($data);
+
+// Close connection
+$conn->close();
+?>
